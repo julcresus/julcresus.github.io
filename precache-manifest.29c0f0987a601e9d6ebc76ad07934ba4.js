31637cc6f402c83e71c23944929a8d720f97d348
Julien Cresus <cresusjulien@gmail.com>
2021-12-09 17:57:37 +0000

    Updates

---
+++ precache-manifest.29c0f0987a601e9d6ebc76ad07934ba4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad449255e3229a59ce51a050c5861f3",
+    "url": "/julcresus.github.io/index.html"
+  },
+  {
+    "revision": "c4e909e019d4c34c1f87",
+    "url": "/julcresus.github.io/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "126cbb9cc3d8728e437c",
+    "url": "/julcresus.github.io/static/css/main.cf6eae6d.chunk.css"
+  },
+  {
+    "revision": "c4e909e019d4c34c1f87",
+    "url": "/julcresus.github.io/static/js/2.02851d16.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/julcresus.github.io/static/js/2.02851d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126cbb9cc3d8728e437c",
+    "url": "/julcresus.github.io/static/js/main.36eb6ae1.chunk.js"
+  },
+  {
+    "revision": "8b24a6c477a0bce4fb88",
+    "url": "/julcresus.github.io/static/js/runtime-main.e57b3d66.js"
+  }
+]);
